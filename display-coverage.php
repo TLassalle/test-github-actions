1ccf3d1e0d10003679a78667b3aa7d1930a0c392
Thomas Lassalle <thomas.lassalle@hotmail.fr>
2022-04-09 12:49:12 +0200

    Add color in test coverage

--- display-coverage.php
+++ display-coverage.php
@@ -6,5 +6,5 @@ $coveredStatements = $xml->project->metrics['coveredstatements'];
 $totalStatements = $xml->project->metrics['statements'];
 $percentage = round(min(1, $coveredStatements / $totalStatements) * 100);
 $percentageString = $percentage.'%';
-echo $percentage;
+echo 100. === $percentage ? "\033[32m$percentageString" : "\033[31m$percentageString";
 unlink('coverage.xml');
